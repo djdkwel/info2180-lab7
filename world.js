51fe9a66598e1df41eb4fc9c5d3e9600fa0fc275
djdkwel <44046752+djdkwel@users.noreply.github.com>
2018-11-19 16:58:10 -0800

    Add files via upload

--- world.js
+++ world.js
@@ -22,19 +22,27 @@ window.onload = function() {
 }//end of doSomething function 
  lookup.onclick = function(){
         var country = document.getElementById("country").value; 
-        if(cBox == true){
-            url = "https://info2180-djdkwel97.c9users.io/world.php?all=true";
-}//end of if statement
+        if(cBox == true && country == ""){
+            url2 = "https://info2180-djdkwel97.c9users.io/world.php?all=true";
+        httpRequest.onreadystatechange = doSomething; 
+        httpRequest.open('GET', url2); 
+        httpRequest.send();
+    }//end of if statement
         else{
         console.log(country);
         url = url + country;
-        }//end of else statement
+        //end of else statement
         console.log(url)
         httpRequest.onreadystatechange = doSomething; 
         httpRequest.open('GET', url); 
         httpRequest.send();
+        }// end of else if 
+        
+       url = "https://info2180-djdkwel97.c9users.io/world.php?q=";
+    
     }// end of lookup onclick function
     
-
+    
+    
     
 }// end of window.onload function 
--- world.php
+++ world.php
@@ -8,6 +8,7 @@ $all = $_GET["all"];
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$Country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 echo '<ul>';
 foreach ($results as $row) {
   echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
